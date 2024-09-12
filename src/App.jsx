49924bcec203efb94bf33e99e73ab81863c61c3f
Praveen Singh <Praveencdg@gmail.com>
2024-09-12 18:32:16 +0530

    feat: Add Navbar with logo and dark mode toggle, and loader during app startup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
+import Loader from "./Components/Loader";
+import Navbar from "./Components/Navbar"
+import { useEffect, useState } from "react";
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
   return (
-    <div>App</div>
+    <div className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
+      <Navbar />
+    </div>
   )
 }
 export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,35 @@
+import ToggleTheme from "./ToggleTheme";
+import { Github } from 'lucide-react';
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-100 dark:bg-gray-800 shadow-md fixed top-0 z-50 w-full">
+      <div className="mx-auto flex items-center justify-between pr-3 backdrop-blur-lg">
+        <div className="text-gray-800 dark:text-white cursor-pointer">
+          <a href="/">
+            <img
+              width={0}
+              height={0}
+              src="/Logo.svg"
+              alt="Logo"
+              style={{ width: '300px', height: "auto" }}
+            />
+          </a>
+        </div>
+
+        <div className="flex items-center md:px-9 px-3 space-x-4">
+          <a
+            href="https://github.com/Praveenskg/EmiCal"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-800 dark:text-white"
+          >
+            <Github className="" />
+          </a>
+          <ToggleTheme />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
