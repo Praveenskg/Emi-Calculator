a955cd0bef7420edf8758193bf597b048010b4de
Praveen Singh <Praveencdg@gmail.com>
2024-09-12 18:35:44 +0530

    feat: Add amortization schedule to EMI form

---
+++ src/Components/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <div className="flex flex-col space-y-2 text-center p-4 rounded-lg mb-2 dark:border dark:border-gray-700 md:border-0 ">
+      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+        Emi Calculator
+      </h3>
+      <p className="text-sm text-muted-foreground mx-3">
+        Empowering Financial Decision-Making: Our EMI Calculator simplifies loan
+        planning by providing accurate Equated Monthly Installment calculations.
+        Tailored for individuals and businesses alike, this tool offers
+        insightful amortization schedules, enabling informed borrowing and
+        repayment strategies. Take charge of your financial future with
+        precision and confidence.
+      </p>
+    </div>
+  );
+};
+
+export default Header;
