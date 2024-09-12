0eb6d10feca2193ff5e014e5f282da2d9bed952f
Praveen Singh <Praveencdg@gmail.com>
2024-09-12 18:34:33 +0530

    feat: Add EMI form with validation using Yup and Formik

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,15 @@ const App = () => {
   return (
     <div className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
       <Navbar />
+      <div className="  flex-1 flex-row">
+        <div className="flex flex-1 flex-row">
+          <div className="flex flex-1 flex-col overflow-y-auto mx-1">
+            <div className="relative flex flex-1 md:px-[300px] md:pt-36 pt-16">
+              <EmiForm />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
